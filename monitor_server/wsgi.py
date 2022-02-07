7d40fb984bd35ffb1818c3d9b29db0510ccf4b62
chatu <chatu@qq.com>
2022-02-07 13:43:46 +0800

    新增部署配置

--- monitor_server/wsgi.py
+++ monitor_server/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitor_server.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitor_server.pro_settings')
 
 application = get_wsgi_application()
