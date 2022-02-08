0479696fe54eadf6f4a3ad49476804f5835f09c4
chatu <chatu@qq.com>
2022-02-08 16:32:52 +0800

    更新相关配置

--- apps/system/admin.py
+++ apps/system/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_title = "服务器监控平台"
+admin.site.site_header = "泛地缘-服务器监控平台"
--- web_start.sh
+++ web_start.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations assets linux mysql rds windows system &&
 python manage.py migrate &&
 #python manage.py runserver 0.0.0.0:8000 > logs/django-web.log 2>&1 &
-nohup python manage.py runserver 0.0.0.0:8888 > logs/django-web.log 2>&1 &
+#nohup python manage.py runserver 0.0.0.0:8888 > logs/django-web.log 2>&1 &
